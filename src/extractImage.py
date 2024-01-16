daee99289901ce49f891447eaf116fd8fd507939
xhoangDA <100712442+xhoangDA@users.noreply.github.com>
2024-01-16 17:22:33 +0700

    Update extractImage.py

--- src/extractImage.py
+++ src/extractImage.py
@@ -44,7 +44,7 @@ def pullImage(imageName):
 
 
 # Hàm để lấy ra tên file/thư mục được đẩy vào trong image
-def extract_folder_from_add_or_copy_syntax(line):
+def extractFolderFromAddOrCopySyntax(line):
     # Tìm kiếm chuỗi "COPY" hoặc "ADD"
     match = re.search(r'\b(COPY|ADD)\b', line)
     if match:
@@ -67,7 +67,7 @@ def extract_folder_from_add_or_copy_syntax(line):
     return None
 
 # Hàm dùng để lấy ra tên đường dẫn tuyệt đối thay thế cho đường dẫn tương đối '.' hoặc './'
-def extract_value_of_dot(lines, i, value):
+def extractValueOfDot(lines, i, value):
     breakPoint = False
     if value == "." or value == "./":
         while breakPoint == False:
@@ -84,30 +84,30 @@ def extract_value_of_dot(lines, i, value):
         return value
 
 # Hàm lấy ra tất cả tên thư mục được thêm vào image
-def get_things_add_to_image(docker_history_array):
+def getThingsAddToImage(docker_history_array):
     final_values = []
     # Lọc giá trị cho từng dòng và in ra kết quả
     for i in range(len(docker_history_array)):
         if i != len(docker_history_array) - 1:
-            value = extract_folder_from_add_or_copy_syntax(docker_history_array[i])
+            value = extractFolderFromAddOrCopySyntax(docker_history_array[i])
             if value is not None:
-                final_values.append(extract_value_of_dot(docker_history_array, i, value))
+                final_values.append(extractValueOfDot(docker_history_array, i, value))
                 final_values = list(set(final_values))
     return final_values
 
 # Hàm để lấy ra những file/thư mục được add thêm vào thư mục / (ngoại trừ các folder OS mặc định)
-def get_new_things_in_root(ls_from_root):
+def getNewThingsInRoot(lsFromRoot):
     root_folder_list = ['bin', 'boot', 'cdrom', 'dev', 'etc', 'home', 'lib', 'lib32', 'lib64', 'libx32', 'lost+found', 'media', 'mnt', 'opt', 'proc', 'root', 'run', 'sbin', 'snap', 'srv', 'sys', 'tmp', 'usr', 'var']
 
     s = set(root_folder_list)
-    result = [x for x in ls_from_root if x not in s]
+    result = [x for x in lsFromRoot if x not in s]
     # Sử dụng vòng lặp để thêm ký tự '/' vào mỗi phần tử trong mảng result
     for i in range(len(result)):
         result[i] = '/' + result[i]
     return result
 
 # Hàm để lấy ra danh sách các files/thư mục trong đường dẫn / (tương đương lệnh "ls -a /")
-def ls_from_root(input):
+def lsFromRoot(input):
     lines = set()  # Sử dụng set để loại bỏ các chuỗi trùng lặp
     for line in input.split('\n'):
         if '/' in line:
@@ -117,7 +117,7 @@ def ls_from_root(input):
 
     return list(lines)
 
-def copy_from_container_to_host(normalizeImage, list_folder, container_id):
+def copyFromContainerToHost(normalizeImage, list_folder, container_id):
     # Tạo thư mục lưu trữ file/thư mục từ image sang host
     # subprocess.getoutput(f"mkdir -p /tmp/checkfilesize/{normalizeImage}")
     subprocess.getoutput(f"[ -d /tmp/checkfilesize/{normalizeImage} ] && rm -rf /tmp/checkfilesize/{normalizeImage}")
@@ -141,7 +141,7 @@ def copy_from_container_to_host(normalizeImage, list_folder, container_id):
     # trả về  đường dẫn lưu trữ files
     return f"/tmp/checkfilesize/{normalizeImage}"
 
-def process_to_copy_from_container_to_host(image_name):
+def processCopyFromContainerToHost(image_name):
     # Chuẩn hóa tên image thành format có thể đặt thành tên folder
     normalizeImage = re.sub('[\/\\-:\s]+', '_', image_name)
 
@@ -149,7 +149,7 @@ def process_to_copy_from_container_to_host(image_name):
     docker_history_output = subprocess.getoutput(f"docker history {image_name} --no-trunc")
     docker_history_output_list = docker_history_output.split("\n")
 
-    things_add_to_image = get_things_add_to_image(docker_history_output_list)
+    things_add_to_image = getThingsAddToImage(docker_history_output_list)
 
     # Tạo container mà không cần start
     container_id = subprocess.getoutput(f"docker create {image_name}")
@@ -161,10 +161,10 @@ def process_to_copy_from_container_to_host(image_name):
         container_list_files = subprocess.getoutput(f"docker export {container_id} | tar t")
 
         # Lấy ra danh sách file/thư mục trong đường dẫn /
-        root_ls = ls_from_root(container_list_files)
+        root_ls = lsFromRoot(container_list_files)
 
         # Lấy ra các file/thư mục khác với các thư mục filesystem mặc định trong đường dẫn /
-        new_things_in_root = get_new_things_in_root(root_ls)
+        new_things_in_root = getNewThingsInRoot(root_ls)
 
         # Loại bỏ / ra khỏi mảng things_add_to_image
         things_add_to_image.remove("/")
@@ -174,7 +174,7 @@ def process_to_copy_from_container_to_host(image_name):
     else:
         final_list_add_to_image = things_add_to_image
         
-    stored_path = copy_from_container_to_host(normalizeImage, final_list_add_to_image, container_id)
+    stored_path = copyFromContainerToHost(normalizeImage, final_list_add_to_image, container_id)
     # print(final_list_add_to_image)
     
     # Xóa các file symbolic links để  tránh xảy ra lỗi
